e41c3cc4da3bf0c6299df6d24312c4ded3c758fc
vogel-sa <svogel2bro@gmail.com>
2016-03-28 13:20:32 -0400

    Added dependencies for bootstrap

--- EZServe/EZServe/settings.py
+++ EZServe/EZServe/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jquery',
+    'bootstrap3',
+    'bootstrap3_datetime',
     'submit_reports',
 ]
 
